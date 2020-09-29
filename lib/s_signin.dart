3da1a0e55f851abb94a6fb6e3f6391c359de786d
joontohub <joonto7828@gmail.com>
2020-09-29 20:22:42 +0900

    Made inputfield and need to make Dataflow to server and oath system, gps system

---
+++ lib/s_signin.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StoreSignIn extends StatefulWidget {
+  @override
+  _StoreSignInState createState() => _StoreSignInState();
+}
+
+class _StoreSignInState extends State<StoreSignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/s_signup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StoreSignUp extends StatefulWidget {
+  @override
+  _StoreSignUpState createState() => _StoreSignUpState();
+}
+
+class _StoreSignUpState extends State<StoreSignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
